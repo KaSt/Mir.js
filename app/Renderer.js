8f73b806ad2d7e454e2275b3f26576e67a20c19b
Dominic Gannaway <dominic@101010digital.com>
2014-12-07 22:34:35 +0000

    got player directions working

--- app/Renderer.js
+++ app/Renderer.js
@@ -28,7 +28,7 @@ Renderer.prototype._render = function() {
 		}
 
 		if(scene !== null && scene.isLoadingMap() === false) {
-			scene.updateAnimations().
+			scene.updateAnimations();
 			this._renderer.render(scene.getStage());
 		};
 
